56e5b0208cdfebef0a44d73b03f6d061a8cf8d2f
kmelamed <kmelamed@buckley.org>
2018-07-13 20:28:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //Code your solution here
+
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+  }
+  
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function increaseRankBy(n) {
+  var children = document.querySelectorAll('ul.ranked-list')
+  for (let i=0; i<children.length; i++) {
+     children[i].innerHTML = parseInt(children[i]) + n
+  }
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
